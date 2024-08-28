893cd595ef104b5f99298bafced566915fc6b399
Kizito25 <kizito25@gmail.com>
2024-08-28 20:37:24 +0100

    Update: changed port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ import mongooseConn from "./config/_config.js";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
